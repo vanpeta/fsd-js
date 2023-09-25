c676e018261bd049e74316aa7ba01f4dd4743be5
vanpeta <carlosallendesanz@gmail.com>
2023-09-25 08:06:42 -0700

    adding segments

--- js/math/graph.js
+++ js/math/graph.js
@@ -8,6 +8,34 @@ class Graph {
         this.points.push(point);
     }
 
+    containsPoint(point) {
+        return this.points.find(p => p.equals(point));
+    }
+
+    tryAddPoint(point) {
+        if(!this.containsPoint(point)) {
+            this.addPoint(point);
+            return true;
+        }
+        return false;
+    }
+
+    addSegment(seg) {
+        this.segments.push(seg);
+    }
+
+    containsSegment(seg) {
+        return this.segments.find((s) => s.equals(seg));
+    }
+
+    tryAddSegment(seg) {
+        if(!this.containsSegment(seg) && !seg.p1.equals(seg.p2)) {
+            this.addSegment(seg);
+            return true;
+        }
+        return false;
+    }
+
     draw(ctx) {
         for (const seg of this.segments) {
             seg.draw(ctx);
